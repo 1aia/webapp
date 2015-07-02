f9d658c5b799ff909909942b948c5298b10cea3b
1aia <ahmetov.almaz@gmail.com>
2015-07-02 09:25:10 +0300

    * keepalive

--- webapp/Global.asax.cs
+++ webapp/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Net;
+using System.Threading;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +16,32 @@ namespace webapp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            KeepAliveThread.Start();
+        }
+
+        static readonly Thread KeepAliveThread = new Thread(KeepAlive);
+
+        protected void Application_End()
+        {
+            KeepAliveThread.Abort();
+        }
+
+        static void KeepAlive()
+        {
+            while (true)
+            {
+                var req = WebRequest.Create("http://aia.apphb.com");
+                req.GetResponse();
+                try
+                {
+                    Thread.Sleep(5*60000);
+                }
+                catch (ThreadAbortException)
+                {
+                    break;
+                }
+            }
         }
     }
 }
